bb1dd5b51a3b8652a2735ed3d9362e029198ea1c
sw2719 <sw2719@naver.com>
2020-01-10 02:38:08 +0900

    Remove unnecessary code

--- steamswitcher.py
+++ steamswitcher.py
@@ -6,12 +6,11 @@ from modules.ui import MainApp
 from modules.reg import fetch_reg
 from modules.account import acc_getlist
 
-print('App Start')
+print('Init start')
 
+__VERSION__ = '1.9.1'
 BRANCH = 'master'
-__VERSION__ = '1.9'
 URL = ('https://raw.githubusercontent.com/sw2719/steam-account-switcher/%s/version.yml' % BRANCH)
-
 
 if getattr(sys, 'frozen', False):
     print('Running in a bundle')
@@ -21,22 +20,14 @@ if getattr(sys, 'frozen', False):
             shutil.rmtree('updater')
         except OSError:
             pass
-else:
-    print('Running in a Python interpreter')
-    BUNDLE = False
-
-print('Running on', os.getcwd())
-
-
-def afterupdate():
     if os.path.isfile('update.zip'):
         try:
             os.remove('update.zip')
         except OSError:
             pass
-
-
-print('Fetching registry values...')
+else:
+    print('Running in a Python interpreter')
+    BUNDLE = False
 
 if fetch_reg('autologin') != 2:
     print('Autologin value is ' + str(fetch_reg('autologin')))
@@ -47,15 +38,12 @@ if fetch_reg('autologin'):
 else:
     print('ERROR: Could not fetch current autologin user!')
 
-
-print('Init complete. Main app starting.')
 root = MainApp(__VERSION__, URL, BUNDLE)
 root.draw_button()
 root.after(100, lambda: start_checkupdate(root, __VERSION__, URL, BUNDLE))
 
-if os.path.isfile(os.path.join(os.getcwd(), 'update.zip')):
-    root.after(150, afterupdate)
 if not acc_getlist():
     root.after(200, root.importwindow)
 
+print('Init complete.')
 root.mainloop()
