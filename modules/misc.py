763b5938873ad5609e795fd65bd490e9ee760438
sw2719 <sw2719@naver.com>
2020-03-23 03:36:45 +0900

    Remove old process checker

--- modules/misc.py
+++ modules/misc.py
@@ -1,22 +1,7 @@
-import psutil
 import winreg
 from modules.reg import fetch_reg
 
 HKCU = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-
-
-def check_running(process_name):
-    '''Check if given process is running and return boolean value.
-    :param process_name: Name of process to check
-    '''
-    for process in psutil.process_iter():
-        try:
-            if process_name.lower() in process.name().lower():
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied,
-                psutil.ZombieProcess):
-            pass
-    return False
 
 
 def steam_running():
