4829770de0eabdab4f813af171739c92788441eb
sw2719 <sw2719@naver.com>
2020-05-06 21:15:56 +0900

    Well, that's pointless

--- steamswitcher.py
+++ steamswitcher.py
@@ -5,7 +5,7 @@ from modules.config import first_run
 from modules.update import start_checkupdate
 from modules.ui import MainApp
 
-__VERSION__ = '2.2.2'
+VERSION = '2.2.2'
 BRANCH = 'master'
 URL = ('https://raw.githubusercontent.com/sw2719/steam-account-switcher/%s/version.yml' % BRANCH)
 
@@ -26,9 +26,9 @@ else:
     print('Running in a Python interpreter')
     BUNDLE = False
 
-root = MainApp(__VERSION__, URL, BUNDLE)
+root = MainApp(VERSION, URL, BUNDLE)
 root.draw_button()
-root.after(100, lambda: start_checkupdate(root, __VERSION__, URL, BUNDLE))
+root.after(100, lambda: start_checkupdate(root, VERSION, URL, BUNDLE))
 
 if first_run:
     root.after(200, root.welcomewindow)
