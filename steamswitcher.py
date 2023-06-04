2f1975d423c1a8f02bb118103b13999ca361f266
Shinwoo Kim <sw2719@naver.com>
2023-06-04 21:26:18 +0900

    Add proper argument parsing

--- steamswitcher.py
+++ steamswitcher.py
@@ -2,21 +2,27 @@ import sys
 import os
 import shutil
 import logging
+import argparse
 from modules.main import MainApp
 
 VERSION = '3.1'
-BRANCH = 'master'
 
 logger = logging.getLogger()
+parser = argparse.ArgumentParser()
 
-if '-logfile' in sys.argv:
-    logging.basicConfig(level=logging.INFO, filename='log.txt')
-else:
-    logging.basicConfig(level=logging.INFO)
+parser.add_argument('-debug', action='store_true', help='Run in debug mode')
+parser.add_argument('-logfile', action='store_true', help='Log to file')
+parser.add_argument('-l', '--log-level', type=str, default='INFO', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='Set log level')
 
+args = parser.parse_args()
+
+if args.logfile:
+    logger.addHandler(logging.FileHandler('log.txt', 'w', 'utf-8'))
+
+logger.setLevel(args.log_level)
 logger.info(f'Launch arguments: {" ".join(sys.argv)}')
 
-if '-debug' in sys.argv:
+if args.debug:
     BUNDLE = False
 elif getattr(sys, 'frozen', False):
     BUNDLE = True
@@ -30,12 +36,10 @@ elif getattr(sys, 'frozen', False):
             os.remove('update.zip')
         except OSError:
             pass
-    logger.info('Running in a bundle')
+    logger.info('Running on an executable')
 else:
     BUNDLE = False
     logger.info('Running in a Python interpreter')
 
-
 root = MainApp(VERSION, BUNDLE)
-
 root.mainloop()
